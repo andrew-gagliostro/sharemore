c1cc762a78037972b0926b5f30e124a7941007b1
andrew-gagliostro <argagliostro@gmail.com>
2021-03-01 19:13:51 -0500

    changed opacity on footer

--- src/components/HomeHead.js
+++ src/components/HomeHead.js
@@ -52,7 +52,7 @@ const HomeHead = () => {
         </header>
         <section className="home-hero">
             <h2>ShareMore</h2>
-            <h3>Working to connect fans and public figures <br />for a greater good</h3>
+            <h3>Working to connect fans and public figures <br />for causes that matter</h3>
         </section>
         </section>
        
