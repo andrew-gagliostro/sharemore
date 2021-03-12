ee79f42d083ee75549a327fd349ba5290828f881
andrew-gagliostro <argagliostro@gmail.com>
2021-03-12 14:20:09 -0500

    put 3 lines as dropwdown face

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -6,79 +6,28 @@ import { Link } from 'react-router-dom';
 
 import { Dropdown } from 'react-bootstrap';
 import AccountLinks from './AccountLinks';
-
-
-//inside of return
-
-
+import { useHistory } from "react-router-dom";
 
 const DropDown = () => {
 
-    /*
-     <Menu id="dropDownMenu">
-        <Dropdown text='Menu'>
-            <Dropdown.Menu>
-            <Dropdown.Item>
-            <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/shop">Shop ShareMore</Link>
-                </li>
-                <li>
-                    <Link to="/doner">Doner/Partner Info</Link>
-                </li>
-            </ul>
-            </div>
-            </Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
-        </Menu>
-    */
-           /*
-        <div id="dropDownMenu">
-        <Menu >
-        <Dropdown text='Menu'>
-            <Dropdown.Menu id="menuContainer">
 
-                <Dropdown.Item id="dropDownItem">
+    const history = useHistory();
 
-                    <Link to="/">Home</Link>
-                
-                </Dropdown.Item>
-                <Dropdown.Item id="dropDownItem">
-
-                    <Link to="/shop">Shop ShareMore</Link>
-
-                </Dropdown.Item>
-
-                <Dropdown.Item id="dropDownItem">
-
-
-                    <Link to="/doner">Doner/Partner Info</Link>
-                </Dropdown.Item>
-
-            </Dropdown.Menu>
-        </Dropdown>
-        </Menu>
-        </div>
-        */
-
-    
-
-    
-    
 
     return (
         
-        <Dropdown id="dropdownMenu">
-        <Dropdown.Toggle id="dropDownMenu"><div>Menu</div></Dropdown.Toggle>
+        <Dropdown>
+        <Dropdown.Toggle id="dropDownMenu">
+        <div class="container">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+        </Dropdown.Toggle>
         <Dropdown.Menu id="homenav" >
-            <Dropdown.Item href="#" id="dropDownItem"><Link to="/">Home</Link></Dropdown.Item>
-            <Dropdown.Item href="#" id="dropDownItem"><Link to="/shop">Shop ShareMore</Link></Dropdown.Item>
-            <Dropdown.Item href="#" id="dropDownItem"><Link to="/doner">Doner/Partner Info</Link></Dropdown.Item>
+            <Dropdown.Item href="#" id="dropDownItem" onClick={() => { history.push('/') }}>Home</Dropdown.Item>
+            <Dropdown.Item href="#" id="dropDownItem" onClick={() => { history.push('/shop') }}><Link to="/shop">Shop ShareMore</Link></Dropdown.Item>
+            <Dropdown.Item href="#" id="dropDownItem" onClick={() => { history.push('/doner') }}><Link to="/doner">Doner/Partner Info</Link></Dropdown.Item>
             <Dropdown.Item href="#" id="dropDownItem"><AccountLinks></AccountLinks></Dropdown.Item>
         </Dropdown.Menu>
         </Dropdown>
@@ -91,4 +40,4 @@ const DropDown = () => {
 
 
 export default DropDown
-                
+    
