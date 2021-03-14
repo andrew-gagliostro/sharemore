5a34677084181db86158d6431aaf0b5b07d4f3f4
andrew-gagliostro <argagliostro@gmail.com>
2021-03-14 18:24:55 -0400

    tried to fix footer for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,16 @@ const Footer = () => {
 
     return (
         <div id="footer">
-            <section id="footer-section">
+            <section id="footer-left">
             <h2> ShareMore </h2>
             </section>
-            <section id="footer-section">
+            <section id="footer-center">
             <div id="footer-div">| Thank you for visiting ShareMore! |</div><br/>
             <div id="footer-div">| Impact |</div><br/>
             <div id="footer-div">| Contact: argagliostro@gmail.com |</div><br/>
             <div id="footer-div">| Get Involved |</div><br/>
             </section>
-            <section id="footer-section">
+            <section id="footer-right">
                <h4><span>&#169;</span>ShareMore, 2020 </h4>
             </section>
         </div>
